3f87dfa94eac9b19a400bd0f9f08d992aafacb54
Kévin HARAMENDY <kevin.haramendy2019@campus-eni.fr>
2021-09-01 13:25:16 +0200

    fonctionnalités de base

---
+++ src/Controller/MainController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Equipe;
+use App\Entity\Personne;
+use App\Repository\EquipeRepository;
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function accueil(EquipeRepository $er, PersonneRepository $pr): Response
+    {
+        $equipes = $er->findAll();
+        $personnes = $pr->findAll();
+        return $this->render('main/accueil.html.twig', [
+            "equipes" => $equipes,
+            "personnes" => $personnes
+        ]);
+    }
+
+
+
+    /*********
+    * EQUIPE *
+    **********/
+
+    /**
+     * @Route("/addTeam", name="ajoutEquipe")
+     */
+    public function ajoutEquipe(Request $req, EntityManagerInterface $em): Response
+    {
+        $e = new Equipe();
+        $e->setNom($req->get("nom"));
+        $em->persist($e);
+        $em->flush();
+
+        return $this->redirectToRoute("accueil");
+    }
+
+    /**
+     * @Route("/delTeam/{id}", name="delEquipe")
+     */
+    public function delEquipe(Equipe $e, EntityManagerInterface $em): Response
+    {
+        foreach ($e->getPersonnes() as $p) {
+            $e->removePersonne($p);
+        }
+        
+        $em->remove($e);
+        $em->flush();
+
+        return $this->redirectToRoute("accueil");
+    }
+
+
+
+    /*********
+    * PERSO  *
+    **********/
+
+    /**
+     * @Route("/addPerso", name="ajoutPersonne")
+     */
+    public function ajoutPersonne(Request $req, EntityManagerInterface $em, EquipeRepository $er): Response
+    {
+        $p = new Personne();
+        $p->setNom($req->get("nom"));
+        $p->setPrenom($req->get("prenom"));
+
+        if ($req->get("team") != 0) {
+            $equipe = $er->findBy(["id" => $req->get("team")]);
+            $p->addEquipe($equipe[0]);
+        }
+
+        $em->persist($p);
+        $em->flush();
+
+        return $this->redirectToRoute("accueil");
+    }
+
+    /**
+     * @Route("/delPerso/{id}", name="delPerso")
+     */
+    public function delPerso(Personne $p, EntityManagerInterface $em): Response
+    {
+        foreach ($p->getEquipes() as $e) {
+            $p->removeEquipe($e);
+        }
+        
+        $em->remove($p);
+        $em->flush();
+
+        return $this->redirectToRoute("accueil");
+    }
+}
